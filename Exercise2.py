2706ca6610aa8899ebb9b04954b667facfa12c2d
Arleigh Dickerson <arleighdickerson@gmail.com>
2014-12-09 09:55:01 -0500

    ...

--- Exercise2.py
+++ Exercise2.py
@@ -9,15 +9,28 @@ def f(t, y):return - 100 * (y - np.cos(t)) - np.sin(t)
 
 def forward(k): #explicit
     ti = 0
-    yi = 0
+    yi = y0
     while ti <= 1:
         yield (ti, yi)
         yi += k * f(ti, yi)
         ti += k 
 
-#derp backward(k): our differential equation is linear, we don't need an iterative method
+def backward(k): # implicit
+    k100 = 100 * k
+    k100Inc = k100 + 1
+    def yNext(tNext,yCurrent):
+        return  (yCurrent + 
+                 (k100 * np.cos(tNext)) - 
+                 (k * np.sin(tNext))
+                ) / k100Inc
+    ti = 0
+    yi = y0
+    while ti <= 1:
+        yield (ti, yi)
+        ti += k 
+        yi = yNext(ti,yi)
 
-for euler in [forward]:
+for euler in [forward,backward]:
     for k in ks:
         (t, y) = zip(*euler(k))
         pl.plot(t, y)
