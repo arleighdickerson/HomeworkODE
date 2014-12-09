6e268c738fef53f22cd6192ca310d33dbb9acb80
Arleigh Dickerson <arleighdickerson@frugalu.com>
2014-12-09 08:56:11 -0500

    Update Exercise2.py
    deeeerrrrrrp

--- Exercise2.py
+++ Exercise2.py
@@ -5,26 +5,19 @@ import numpy as np
 from matplotlib import pyplot as pl
 ks = [0.01, 0.0125, 0.02, 0.025]
 y0 = 0
-h = 0.019
-def F(t, y):return - 100 * (y - np.cos(t)) - np.sin(t)
+def f(t, y):return - 100 * (y - np.cos(t)) - np.sin(t)
 
-def explicit(k):
+def forward(k): #explicit
     ti = 0
     yi = 0
     while ti <= 1:
         yield (ti, yi)
-        yi += k * F(ti, yi)
+        yi += k * f(ti, yi)
         ti += k 
 
-def implicit(k):
-    ti = 1
-    yi = 1
-    while ti >= 0:
-        yield (ti, yi)
-        yi -= yi + h * F(ti, yi)
-        ti -= k 
+#derp backward(k): our differential equation is linear, we don't need an iterative method
 
-for euler in [implicit, explicit]:
+for euler in [forward]:
     for k in ks:
         (t, y) = zip(*euler(k))
         pl.plot(t, y)
